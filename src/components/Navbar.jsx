c8f24e8b9c556a5c2c9fc8dc6ed208ecee3afb9c
Blake McKenzie <blakemckenzie365@gmail.com>
2023-01-05 17:40:30 +0000

    html for news page built using react components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar () {
+  return (
+    <nav>
+      <h1>W.</h1>
+      <ul>Home</ul>
+      <ul>New</ul>
+      <ul>Popular</ul>
+      <ul>Trending</ul>
+      <ul>Categories</ul>
+    </nav>
+  )
+}
---
+++ src/components/NewArticles.jsx
@@ -0,0 +1,13 @@
+export default function NewArticles () {
+  return (
+    <div>
+      <h1>New</h1>
+      <h2>Hydrogen vs Electric Cars</h2>
+      <p>Will Hydrogen-fueled cars ever catch up to EVs</p>
+      <h2>The Downsides of Ai Artistry</h2>
+      <p>What are the possible adverse effects of on-demand AI image generation?</p>
+      <h2>Is VC Funding Drying up?</h2>
+      <p>Private funding by VC firms is down 50% YOY. We take a look at what means</p>
+    </div>
+  )
+}
