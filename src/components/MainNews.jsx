2475e8c4108a1f1d0d5c08eefe9a22e7ddb33639
Blake McKenzie <blakemckenzie365@gmail.com>
2023-01-06 13:11:43 +0000

    navbar styling and main news content styling added to components

--- src/components/MainNews.jsx
+++ src/components/MainNews.jsx
@@ -1,11 +1,15 @@
 export default function MainNews () {
   return (
-    <div>
-      <img src="src/assets/images/image-web-3-desktop.jpg" alt="main-news-picture" />
-      <h1>The Bright Future of Web 3.0</h1>
-      <div>
-        <p>We dive into the next evolution of the web that claims to put the power of platforms back into the hands of the people. But is it really fulfilling its promise?</p>
-        <button>READ MORE</button>
+    <div className="main-news-container">
+      <div className="main-image-container">
+        <img src="src/assets/images/image-web-3-desktop.jpg" alt="main-news-picture" className="main-image"/>
+      </div>
+      <div className="main-news-text">
+        <h1 className="main-news-prompt">The Bright Future of Web 3.0?</h1>
+        <div>
+          <h2 className="main-news-question">We dive into the next evolution of the web that claims to put the power of platforms back into the hands of the people. But is it really fulfilling its promise?</h2>
+          <button>READ MORE</button>
+        </div>
       </div>
     </div>
   )
